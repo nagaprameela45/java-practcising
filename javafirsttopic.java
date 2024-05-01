5c5223a814514e185cf5e85f01a2d5b189299b3a
nagaprameela45 <130998594+nagaprameela45@users.noreply.github.com>
2024-05-01 19:47:39 +0530

    Add files via upload

---
+++ javafirsttopic.java
@@ -0,0 +1,22 @@
+MAY-1-2024
+
+1)WHAT IS JAVA?
+.JAVA DEFINITION
+.JAVA APPLICATION
+.TYPES OF JAVA APPLICATIONS
+.JAVA PLATFORMS/EDITIONS
+
+
+
+1)JAVA DEFINITION : java is a high level programming language and it is a platform independent language and it is a secure programming language
+.PLATFORM : Any hardware or a software system in which the program runs is know as the platform.since java as the runtime environment it is called as the palatform
+
+
+2)JAVA APPLICATIONS : java ca be used widely used applications.some of them are as folllows:
+.Desktop applications such as media player,antivirus etc.
+.web based applications such as irctcco.in,javatpoint.com.
+.embedded systems
+.Banking applications
+.mobiles
+.Robotics
+.gaming
