c2a31b584681b8e78909ea75675767aa5c2781a8
nagaprameela45 <130998594+nagaprameela45@users.noreply.github.com>
2024-05-02 10:49:59 +0530

    Add files via upload

---
+++ TYPES OF JAVA APPLICATIONS.java
@@ -0,0 +1,14 @@
+TYPES OF JAVA APPLICATIONS 
+.STANDLONE APPLICATION/DESKTOP APPLICATION
+.WEB APPLICATION
+.ENTERPRISE APPLICATION
+.MOBILE APPLICATION
+
+
+1)Standlone aplication : Standlone application is known as has the desktop application it is a traditional software which we need to install on every machine the best example of standlone application are media player,antivirus etc.awt and swing are used in standlone application.
+
+2)Web application : web applications are udes for creating for creating a dynamic a web pages ,Servelts jsp ,hibernate,spring are used in web applications.
+
+3)Enterprise applincation : An application that is distributed in nature is known as the enterprise application the advantage of this application is high level security,load balancing,clustering. an ejb is used for creating enterprise applications.
+
+4)Mobile application : an application which is created for mobile devices is called as an mobile application.an android and java ME are used for creating mobile applications.
